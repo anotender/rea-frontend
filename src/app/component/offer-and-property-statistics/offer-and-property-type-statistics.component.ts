79de69986f8cb26f718bed055f8c4a32862b3b7a
Mateusz Nowak <mateusznowak@vp.pl>
2019-04-23 22:08:07 +0200

    Added line chart visualisation

--- src/app/component/offer-and-property-statistics/offer-and-property-type-statistics.component.ts
+++ src/app/component/offer-and-property-statistics/offer-and-property-type-statistics.component.ts
@@ -14,10 +14,40 @@ export class OfferAndPropertyTypeStatisticsComponent implements OnInit {
   @Input()
   public historicalStatistics: Map<Date, StatisticsByOfferAndPropertyType>;
 
+  public historicalStatisticsData: any[] = [];
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  showNumberOfOffersHistoricalStatistics() {
+    this.historicalStatisticsData = this.getHistoricalStatisticsData(s => s.numberOfOffers, 'Number of offers')
+  }
+
+  showAveragePricePerSquareMeterHistoricalStatistics() {
+    this.historicalStatisticsData = this.getHistoricalStatisticsData(s => s.averagePricePerSquareMeter, 'Average price per square meter')
+  }
+
+  private getHistoricalStatisticsData(property: (StatisticsByOfferAndPropertyType) => number, propertyName: string): any[] {
+    let historicalStatisticsSeries: any[] = [];
+
+    this.historicalStatistics.forEach((s, d) => {
+      historicalStatisticsSeries.push({
+        name: this.formatDate(d),
+        value: property(s)
+      })
+    });
+
+    return [{
+      name: propertyName,
+      series: historicalStatisticsSeries
+    }];
+  }
+
+  private formatDate(d: Date): string {
+    return d["dayOfMonth"] + ' ' + d["month"] + ' ' + d["year"];
   }
 
 }
